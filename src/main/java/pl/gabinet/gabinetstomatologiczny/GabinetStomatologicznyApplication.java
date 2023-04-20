c309046b0137a073ea8fdc665bcc88d23b9d3c32
Bartek Pr <bartekpretnik@gmail.com>
2023-04-20 21:53:06 +0200

    Initial commit

---
+++ src/main/java/pl/gabinet/gabinetstomatologiczny/GabinetStomatologicznyApplication.java
@@ -0,0 +1,13 @@
+package pl.gabinet.gabinetstomatologiczny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabinetStomatologicznyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabinetStomatologicznyApplication.class, args);
+    }
+
+}
